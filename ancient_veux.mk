5479b7c65028e7baf1fd5a5fd5dd9ec6d91d7692
anant-goel <100245626+anant-goel@users.noreply.github.com>
2023-01-20 07:55:31 +0530

    Bring-UP Ancient-ify

--- ancient_veux.mk
+++ ancient_veux.mk
@@ -10,19 +10,25 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from veux device
 $(call inherit-product, device/xiaomi/veux/device.mk)
+$(call inherit-product, vendor/miuicamera/config.mk)
 
-# Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+# Inherit some common ancient stuff.
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 TARGET_USES_AOSP_RECOVERY := true
+ANCIENT_OFFICIAL := true
+ANCIENT_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
+EXTRA_UDFPS_ANIMATIONS := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_USES_BLUR := true
+TARGET_SUPPORTS_NOW_PLAYING := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
 
-# Evolution-X stuff.
-EVO_BUILD_TYPE := OFFICIAL
-
-PRODUCT_NAME := evolution_veux
+PRODUCT_NAME := ancient_veux
 PRODUCT_DEVICE := veux
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
--- device.mk
+++ device.mk
@@ -316,6 +316,8 @@ PRODUCT_PACKAGES += \
    TelephonyOverlayVeux \
    WifiOverlayVeux
 
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
