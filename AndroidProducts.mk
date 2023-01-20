e68c442c42ef5be1b28c2e90a72f0296838aa090
anant-goel <100245626+anant-goel@users.noreply.github.com>
2023-01-20 07:55:31 +0530

    veux: Ancient-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,10 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_veux.mk
+    $(LOCAL_DIR)/ancient_veux.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_veux-user \
-    aosp_veux-userdebug \
-    aosp_veux-eng
+    ancient_veux-user \
+    ancient_veux-userdebug \
+    ancient_veux-eng
+
--- ancient_veux.mk
+++ ancient_veux.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2022 The AncientOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,19 +8,26 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-TARGET_USES_AOSP_RECOVERY := true
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
-TARGET_SUPPORTS_QUICK_TAP := true
-
 # Inherit from veux device
 $(call inherit-product, device/xiaomi/veux/device.mk)
 
-# Inherit some common PixelExperience
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common ancient stuff.
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
+TARGET_USES_AOSP_RECOVERY := true
+ANCIENT_OFFICIAL := true
+ANCIENT_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_USES_BLUR := true
+TARGET_SUPPORTS_NOW_PLAYING := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
 
-PRODUCT_NAME := aosp_veux
+PRODUCT_NAME := ancient_veux
 PRODUCT_DEVICE := veux
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
